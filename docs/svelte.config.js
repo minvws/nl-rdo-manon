e9d440a819a501726d3a8e99e27e657513f00f79
Peter-Paul van Gemerden <mail@ppvg.nl>
2024-01-15 11:44:50 +0100

    chore: update dependencies (#459)

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { importAssets } from "svelte-preprocess-import-assets";
 
 /** @type {import('@sveltejs/kit').Config} */
