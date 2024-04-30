87032d1bcec7009e9331514d8dc71216f338de12
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-04-30 11:53:47 +0200

    Let there be manon

--- examples/django/django-project/app/app/settings.py
+++ examples/django/django-project/app/app/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
