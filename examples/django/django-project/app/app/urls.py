aa48213a9cf527e72f057fb03fa5e53b55ac165b
JP Bruins Slot <jpbruinsslot@gmail.com>
2024-05-06 18:05:25 +0200

    Add npm as build step and start with example app

--- examples/django/django-project/app/app/urls.py
+++ examples/django/django-project/app/app/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
