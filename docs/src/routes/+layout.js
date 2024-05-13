0bff743086a8602174d0692f9c19d35ecb990830
Peter-Paul van Gemerden <mail@ppvg.nl>
2024-05-13 21:20:30 +0200

    ci: fix github pages docs redirects (apply BASE_PATH) (#540)

--- docs/src/routes/+layout.js
+++ docs/src/routes/+layout.js
@@ -1,12 +1,16 @@
 import { redirect } from "@sveltejs/kit";
 import { redirects } from "../redirects.js";
+import { base } from "$app/paths";
 
 export const prerender = true;
 
 /** @type {import('./$types').LayoutLoad} */
 export function load({ url }) {
-  const { pathname } = url;
+  let { pathname } = url;
+  if (base && pathname.startsWith(base)) {
+    pathname = pathname.replace(base, "");
+  }
   if (pathname in redirects) {
-    return redirect(301, redirects[pathname]);
+    return redirect(301, base + redirects[pathname]);
   }
 }
