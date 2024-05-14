783bed40e801d2cd634372e6efd6c92a753975fe
Peter-Paul van Gemerden <mail@ppvg.nl>
2024-05-14 10:32:05 +0200

    docs: update table checkbox accessibility instructions (#538)
    * docs: table checkbox accessibility
    
    * docs: improve table checkbox accessibility instructions
    
    * docs: update table-checkbox-test page as well

--- docs/src/routes/(docs)/components/table-checkbox-test/+page.svelte
+++ docs/src/routes/(docs)/components/table-checkbox-test/+page.svelte
@@ -25,24 +25,41 @@
         <div class="horizontal-scroll">
           <form>
             <table>
-              <caption> Tabelvoorbeeld met selectievak: </caption>
+              <caption>
+                Tabelvoorbeeld met selectievak:
+                <span class="visually-hidden">
+                  Selectievak in kolom 1 rij 1 selecteert alle rijen.
+                </span>
+              </caption>
               <thead>
                 <tr>
-                  <th scope="col"><input type="checkbox" /></th>
-                  <th scope="col">Table header heading</th>
-                  <th scope="col">Action</th>
+                  <th scope="col">
+                    <span class="visually-hidden">Selectie</span>
+                    <input type="checkbox" />
+                  </th>
+                  <th scope="col">Naam</th>
+                  <th scope="col">Actie</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td><input type="checkbox" /></td>
-                  <td>Ipsum</td>
-                  <td>
-                    <form><input type="submit" value="Lorem" /></form>
-                  </td>
+                  <td><input type="checkbox" id="lorem-input-1" /></td>
+                  <td><label for="lorem-input-1">Lorem</label></td>
+                  <td><button type="button">Actie uitvoeren</button></td>
+                </tr>
+                <tr>
+                  <td><input type="checkbox" id="ipsum-input-1" /></td>
+                  <td><label for="ipsum-input-1">Ipsum</label></td>
+                  <td><button type="button">Actie uitvoeren</button></td>
+                </tr>
+                <tr>
+                  <td><input type="checkbox" id="dolor-input-1" /></td>
+                  <td><label for="dolor-input-1">Dolor</label></td>
+                  <td><button type="button">Actie uitvoeren</button></td>
                 </tr>
               </tbody>
             </table>
+            <button type="submit">Actie uitvoeren op geselecteerde items</button>
           </form>
         </div>
 
@@ -52,23 +69,44 @@
           code={`
 <div class="horizontal-scroll">
   <form>
-    <table>
-      <caption>Tabelvoorbeeld met selectievak:</caption>
-      <thead>
-        <tr>
-          <th scope="col"><input type="checkbox"></th>
-          <th scope="col">Table header heading</th>
-          <th scope="col">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><input type="checkbox"></td>
-          <td>Ipsum</td>
-          <td><form><input type="submit" value="Lorem"></form></td>
-        </tr>
-      </tbody>
-    </table>
+    <form>
+      <table>
+        <caption>
+          Tabelvoorbeeld met selectievak:
+          <span class="visually-hidden">
+            Selectievak in kolom 1 rij 1 selecteert alle rijen.
+          </span>
+        </caption>
+        <thead>
+          <tr>
+            <th scope="col">
+              <span class="visually-hidden">Selectie</span>
+              <input type="checkbox" />
+            </th>
+            <th scope="col">Naam</th>
+            <th scope="col">Actie</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><input type="checkbox" id="lorem-input-1"/></td>
+            <td><label for="lorem-input-1">Lorem</label></td>
+            <td><button type="button">Actie uitvoeren</button></td>
+          </tr>
+          <tr>
+            <td><input type="checkbox" id="ipsum-input-1"/></td>
+            <td><label for="ipsum-input-1">Ipsum</label></td>
+            <td><button type="button">Actie uitvoeren</button></td>
+          </tr>
+          <tr>
+            <td><input type="checkbox" id="dolor-input-1"/></td>
+            <td><label for="dolor-input-1">Dolor</label></td>
+            <td><button type="button">Actie uitvoeren</button></td>
+          </tr>
+        </tbody>
+      </table>
+      <button type="submit">Actie uitvoeren op geselecteerde items</button>
+    </form>
   </form>
 </div>
 `}
@@ -78,28 +116,41 @@
         <h4>Visueel voorbeeld:</h4>
         <div class="horizontal-scroll">
           <table>
-            <caption> Tabelvoorbeeld met selectievak: </caption>
+            <caption>
+              Tabelvoorbeeld met selectievak:
+              <span class="visually-hidden">
+                Selectievak in kolom 1 rij 1 selecteert alle rijen.
+              </span>
+            </caption>
             <thead>
               <tr>
                 <th scope="col">
+                  <span class="visually-hidden">Selectie</span>
                   <form><input type="checkbox" /></form>
                 </th>
-                <th scope="col">Table header heading</th>
-                <th scope="col">Action</th>
+                <th scope="col">Naam</th>
+                <th scope="col">Actie</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>
-                  <form><input type="checkbox" /></form>
-                </td>
-                <td>Ipsum</td>
-                <td>
-                  <form><input type="submit" value="Lorem" /></form>
-                </td>
+                <td><form><input type="checkbox" id="lorem-input-2" /></form></td>
+                <td><label for="lorem-input-2">Lorem</label></td>
+                <td><button type="button">Actie uitvoeren</button></td>
+              </tr>
+              <tr>
+                <td><form><input type="checkbox" id="ipsum-input-2" /></form></td>
+                <td><label for="ipsum-input-2">Ipsum</label></td>
+                <td><button type="button">Actie uitvoeren</button></td>
+              </tr>
+              <tr>
+                <td><form><input type="checkbox" id="dolor-input-2" /></form></td>
+                <td><label for="dolor-input-2">Dolor</label></td>
+                <td><button type="button">Actie uitvoeren</button></td>
               </tr>
             </tbody>
           </table>
+          <button type="submit">Actie uitvoeren op geselecteerde items</button>
         </div>
 
         <h4>HTML-voorbeeld:</h4>
@@ -108,22 +159,41 @@
           code={`
 <div class="horizontal-scroll">
   <table>
-    <caption>Tabelvoorbeeld met selectievak:</caption>
+    <caption>
+      Tabelvoorbeeld met selectievak:
+      <span class="visually-hidden">
+        Selectievak in kolom 1 rij 1 selecteert alle rijen.
+      </span>
+    </caption>
     <thead>
       <tr>
-        <th scope="col"><form><input type="checkbox"></form></th>
-        <th scope="col">Table header heading</th>
-        <th scope="col">Action</th>
+        <th scope="col">
+          <span class="visually-hidden">Selectie</span>
+          <form><input type="checkbox" /></form>
+        </th>
+        <th scope="col">Naam</th>
+        <th scope="col">Actie</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td><form><input type="checkbox"></form></td>
-        <td>Ipsum</td>
-        <td><form><input type="submit" value="Lorem"></form></td>
+        <td><form><input type="checkbox" id="lorem-input-2" /></form></td>
+        <td><label for="lorem-input-2">Lorem</label></td>
+        <td><button type="button">Actie uitvoeren</button></td>
+      </tr>
+      <tr>
+        <td><form><input type="checkbox" id="ipsum-input-2" /></form></td>
+        <td><label for="ipsum-input-2">Ipsum</label></td>
+        <td><button type="button">Actie uitvoeren</button></td>
+      </tr>
+      <tr>
+        <td><form><input type="checkbox" id="dolor-input-2" /></form></td>
+        <td><label for="dolor-input-2">Dolor</label></td>
+        <td><button type="button">Actie uitvoeren</button></td>
       </tr>
     </tbody>
   </table>
+  <button type="submit">Actie uitvoeren op geselecteerde items</button>
 </div>
 `}
         />
