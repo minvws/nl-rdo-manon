9ba07125f96d43199f788df83831251470c487a3
Peter-Paul van Gemerden <mail@ppvg.nl>
2024-07-22 20:10:49 +0200

    fix: breadcrumb base path (#599)

--- icore_open_docs/src/lib/BreadcrumbNav.svelte
+++ icore_open_docs/src/lib/BreadcrumbNav.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { base } from "$app/paths";
   import { page } from "$app/stores";
   import { getBreadcrumbs } from "$lib/breadcrumbs.js";
   import NavLink from "./NavLink.svelte";
@@ -10,7 +11,7 @@
     <div>
       <ul>
         {#each breadcrumbs as crumb}
-          <li><NavLink href={crumb.route}>{crumb.name}</NavLink></li>
+          <li><NavLink href="{base}{crumb.route}">{crumb.name}</NavLink></li>
         {/each}
       </ul>
     </div>
