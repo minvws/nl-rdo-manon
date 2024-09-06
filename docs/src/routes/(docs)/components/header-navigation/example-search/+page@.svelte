444628cfa4576d69b4ad391bdde3b8c7f5285da7
Heleen <magned@gmail.com>
2024-09-06 13:20:35 +0200

    Adds search icon

--- docs/src/routes/(docs)/components/header-navigation/example-search/+page@.svelte
+++ docs/src/routes/(docs)/components/header-navigation/example-search/+page@.svelte
@@ -33,14 +33,13 @@ See: https://kit.svelte.dev/docs/advanced-routing#advanced-layouts-breaking-out-
     aria-label="Hoofdnavigatie"
     class="collapsible"
   >
-    <button type="submit" class="icon-only icon-cat">Zoeken</button>
     <div class="collapsing-element">
       <SiteNavLinks />
 
       <div>
         <form class="inline">
           <input type="text" placeholder="Zoeken" />
-          <button type="submit" class="icon-only icon-cat">Zoeken</button>
+          <button type="submit" class="icon-only icon-search">Zoeken</button>
         </form>
       </div>
     </div>
@@ -76,33 +75,26 @@ See: https://kit.svelte.dev/docs/advanced-routing#advanced-layouts-breaking-out-
           language="html"
           code={`
 <header>
-  <div>
-      <a href="#main-content" class="button focus-only skip-to-content">Ga direct naar inhoud</a>
+  <a href="#main-content" class="button focus-only skip-to-content">Ga direct naar inhoud</a>
 
-      <a href="./logo" class="logo">
-          <img src="/img/logo-white.svg" alt="Placeholder logo">Manon
-      </a>
+  <nav
+    data-open-label="Menu"
+    data-close-label="Sluit menu"
+    data-media="(min-width:42rem)"
+    aria-label="Hoofdnavigatie"
+    class="collapsible"
+  >
+    <div class="collapsing-element">
+      <SiteNavLinks />
 
-      <nav
-      data-open-label="Menu"
-      data-close-label="Sluit menu"
-      data-media="(min-width: 30rem)"
-      aria-label="Hoofdnavigatie"
-      class="collapsible">
-
-        <div class="collapsing-element">
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/components">Componenten</a></li>
-              <li><a href="/documentation">Documentatie</a></li>
-            </ul>
-
-            <ul>
-              <li><a href="#">Logout</a></li>
-            </ul>
-        </div>
-      </nav>
-  </div>
+      <div>
+        <form class="inline">
+          <input type="text" placeholder="Zoeken" />
+          <button type="submit" class="icon-only icon-search">Zoeken</button>
+        </form>
+      </div>
+    </div>
+  </nav>
 </header>
 `}
         />
