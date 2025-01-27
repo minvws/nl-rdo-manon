125b75040a7221d11298fc13ea42a547c575e15e
Peter-Paul van Gemerden <mail@ppvg.nl>
2025-01-27 15:22:06 +0100

    refactor(docs): use iframes instead of in-page examples (#750)
    * refactor(docs): use iframes instead of in-page header/sidemenu examples
    
    * refactor(docs): remove (docs) route group
    
    * fix: header-navigation heading levels
    
    Co-authored-by: Rein Schaap <rein@specs.nl>
    
    * fix: header-navigation heading levels
    
    Co-authored-by: Rein Schaap <rein@specs.nl>
    
    * fix: sidemenu-in-page-exampandable example markup
    
    Co-authored-by: Rein Schaap <rein@specs.nl>
    
    * fix: remove colons from headings
    
    Co-authored-by: Rein Schaap <rein@specs.nl>
    
    * fix: sidemenu page titles and headings
    
    Co-authored-by: Rein Schaap <rein@specs.nl>
    
    * fix: sidemenu in-page-collapsible page heading
    
    Co-authored-by: Rein Schaap <rein@specs.nl>
    
    * chore: remove unused imports
    
    * fix: use example links in examples
    
    * fix: header-navigation iframe heights
    
    * fix: examples/sidemenu-in-page regression
    
    * fix: rename first header-navigation example "Only navigation"
    
    ---------
    
    Co-authored-by: Rein Schaap <rein@specs.nl>

---
+++ docs/src/routes/examples/sidemenu-next-to-page-collapsible/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { page } from "$app/state";
+  import { sidemenuAction } from "$lib/sidemenuAction";
+  import { initSidemenus } from "$lib/manon";
+  onMount(initSidemenus);
+</script>
+
+<svelte:body use:sidemenuAction />
+
+<nav data-open-label="Zijbalknavigatie" data-close-label="Sluit zijbalknavigatie">
+  <ul>
+    <li><a href={page.url.pathname}>Voorbeeld-link 1</a></li>
+    <li><a href={page.url.pathname}>Voorbeeld-link 2</a></li>
+    <li><a href={page.url.pathname}>Voorbeeld-link 3</a></li>
+    <li><a href={page.url.pathname}>Voorbeeld-link 4</a></li>
+  </ul>
+</nav>
+<div class="page-content">
+  <header class="page-header">
+    <div>
+      <nav>
+        <div class="collapsing-element">
+          <ul>
+            <li><a href={page.url.pathname}>Home</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <main>
+    <article class="visually-grouped">
+      <div><section>Main</section></div>
+    </article>
+  </main>
+  <footer>Footer</footer>
+</div>
