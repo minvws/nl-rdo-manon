ddd356411a2b4727027bbaa0ecfbe8f15f76c94a
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-07-22 14:00:25 +0200

    Add test and test runner to manon-sass (#874)
    * Testing sass mixins and functions
    
    * Rename
    
    * Remove tests
    
    * Clean up

---
+++ manon/tests/sass.test.js
@@ -0,0 +1,23 @@
+import { describe, it } from 'vitest';
+import sass from 'sass';
+import path from 'path';
+import sassTrue from 'sass-true';
+
+describe('Sass Tests', () => {
+  const sassTestFiles = [
+    'functions/_validators.test.scss',
+    'mixins/_theming.test.scss',
+    // Add other Sass test files here as you create them
+  ];
+
+  sassTestFiles.forEach((file) => {
+    const filePath = path.resolve(__dirname, file);
+    sassTrue.runSass({ describe, it }, filePath, {
+      sass: sass,
+      includePaths: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, '../'),
+      ],
+    });
+  });
+});
