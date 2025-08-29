823292f33680de0d0eaec6f81ef85b4ca208c01a
HeleenSG <magned@gmail.com>
2025-08-29 14:25:01 +0200

    feat: KAT theme styling (#974)
    * Adds kat theme styling
    
    * feat: add fonts to KAT theme (#975)
    
    * Adds kat styling
    
    * Kat styling
    
    * Adds KAT theme styling
    
    * Adds KAT theme styling
    
    * style: fix lint issue in `table-expando-rows.scss`
    
    * Fixes sass deprecation warnings
    
    * Removes old page reference
    
    * style: fix SCSS formatting for button border-radius in Kat theme
    
    ---------
    
    Co-authored-by: Peter-Paul van Gemerden <mail@ppvg.nl>
    Co-authored-by: Randhir Gulraj <randh1r@hotmail.com>

---
+++ docs/src/routes/components/button-secondary/+page.svelte
@@ -0,0 +1,89 @@
+<script module>
+  export const breadcrumb = "Secondary button";
+</script>
+
+<script lang="ts">
+  import { base } from "$app/paths";
+  import Code from "$lib/Code.svelte";
+  import SideMenu from "$lib/SideMenu.svelte";
+</script>
+
+<svelte:head>
+  <title>Secondary button</title>
+</svelte:head>
+
+<main class="sidemenu" id="main-content" tabindex="-1">
+  <SideMenu>
+    <ul>
+      <li><a href="#introduction">Introductie</a></li>
+      <li><a href="#examples">Voorbeelden</a></li>
+      <li><a href="#requirements">Bijbehorende bestanden</a></li>
+      <li><a href="#related">Gerelateerde pagina's</a></li>
+    </ul>
+  </SideMenu>
+
+  <article id="button-secondary">
+    <div>
+      <section id="introduction">
+        <h1>Secondary button</h1>
+        <p>Subtiele knopweergave.</p>
+
+        <ol>
+          <li>
+            Voeg de class <code>secondary</code> toe om gebruik te maken van de visuele weergave van
+            een "Secondary button".
+          </li>
+        </ol>
+      </section>
+
+      <section id="examples">
+        <h2><code>button</code></h2>
+        <h3>Visuele weergave:</h3>
+
+        <button class="secondary">Lorem ipsum</button>
+
+        <h3>HTML-voorbeeld:</h3>
+        <Code
+          language="html"
+          code={`
+<button class="secondary">Lorem ipsum</button>
+`}
+        />
+      </section>
+
+      <section id="button-secondary-input">
+        <h2><code>input type="button"</code></h2>
+        <h3>Visuele weergave:</h3>
+
+        <input type="button" value="Lorem ipsum" class="secondary" />
+
+        <h3>HTML-voorbeeld:</h3>
+        <Code
+          language="html"
+          code={`
+<input type="button" value="Lorem ipsum" class="secondary">
+`}
+        />
+      </section>
+
+      <section id="requirements">
+        <h2>Bijbehorende bestanden</h2>
+        <p>
+          Voor meer informatie over importeren en instellen van componenten. Zie:
+          <a href="{base}/documentation/import-styling"
+            >Componenten gebruiken en styling toevoegen</a
+          >
+        </p>
+
+        <h3>Importeer component via npm</h3>
+        <h4>CSS-voorbeeld:</h4>
+        <Code
+          language="css"
+          code={`
+      @use "@minvws/manon/button-secondary";
+      `}
+        />
+      </section>
+    </div>
+  </article>
+</main>
