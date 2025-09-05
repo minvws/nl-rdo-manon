526c4ba10d6f60ff818907b76f6fc8f5d17caa7e
Heleen <magned@gmail.com>
2025-09-05 10:53:24 +0200

    Removes old documentation and adds footer example

---
+++ docs/src/routes/examples/footer/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { currentPage } from "$lib/currentPage.svelte";
+  const current = currentPage();
+</script>
+
+<footer>
+  <nav aria-labelledby="footer-nav-1-heading">
+    <h3 id="footer-nav-1-heading">Lorem ipsum</h3>
+    <ul>
+      <li><a href="footer" aria-current="page">Dolor</a></li>
+      <li><a href="footer">Sit</a></li>
+      <li><a href="footer">Amet</a></li>
+    </ul>
+  </nav>
+</footer>
