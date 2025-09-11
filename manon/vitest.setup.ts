686c5f2b6c365ce56eaa4ad70f9fbc37f8ebbfd4
Peter-Paul van Gemerden <mail@ppvg.nl>
2025-09-11 14:54:58 +0200

    chore: make jest-dom matcher types available to typescript

--- manon/vitest.setup.ts
+++ manon/vitest.setup.ts
@@ -2,15 +2,9 @@ import { afterEach } from "vitest";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/vitest";
 
-/**
- * @import {UserEvent} from "@testing-library/user-event"
- */
+(window as any).PointerEvent = MouseEvent;
 
-/**
- * @param {String} markup
- * @return {{ container: HTMLElement, user: UserEvent }}
- */
-export function render(markup) {
+export function render(markup: string) {
   const user = userEvent.setup();
   const container = document.createElement("test-container");
   container.innerHTML = markup;
