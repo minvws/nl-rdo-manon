cd8a22665f622d5bbd4fc1c24cf8f254b404d3bb
rievenmartis <rsnmartis@gmail.com>
2025-10-03 12:24:02 +0200

    fix navigation example pages

--- docs/src/routes/examples/pages/landing/+page@.svelte
+++ docs/src/routes/examples/pages/landing/+page@.svelte
@@ -3,24 +3,7 @@
   const baseUrl = `${base}/examples/pages/landing`;
 </script>
 
-<header>
-  <a href="#main-content" class="button focus-only skip-to-content">Ga direct naar inhoud</a>
-  <nav
-    data-open-label="Menu"
-    data-close-label="Sluit menu"
-    data-media="(min-width: 30rem)"
-    aria-label="Hoofdnavigatie"
-    class="collapsible"
-  >
-    <div class="collapsing-element">
-      <ul>
-        <li><a href={baseUrl} aria-current="page">Home</a></li>
-        <li><a href={baseUrl}>About</a></li>
-        <li><a href={baseUrl}>Contact</a></li>
-      </ul>
-    </div>
-  </nav>
-</header>
+
 
 <main id="main-content" tabindex="-1">
   <section class="hero">
--- docs/src/routes/examples/pages/login/+page@.svelte
+++ docs/src/routes/examples/pages/login/+page@.svelte
@@ -5,21 +5,15 @@
 
 <header>
   <a href="#main-content" class="button focus-only skip-to-content">Ga direct naar inhoud</a>
-  <nav
-    data-open-label="Menu"
-    data-close-label="Sluit menu"
-    data-media="(min-width: 30rem)"
-    aria-label="Hoofdnavigatie"
-    class="collapsible"
-  >
-    <div class="collapsing-element">
-      <ul>
-        <li><a href={baseUrl}>Home</a></li>
-        <li><a href={baseUrl}>About</a></li>
-        <li><a href={baseUrl}>Contact</a></li>
-      </ul>
-    </div>
-  </nav>
+  <nav>
+      <div>
+        <ul>
+          <li><a href={baseUrl} aria-current="page">Home</a></li>
+          <li><a href={baseUrl}>About</a></li>
+          <li><a href={baseUrl}>Contact</a></li>
+        </ul>
+      </div>
+    </nav>
 </header>
 
 <main id="main-content" tabindex="-1">
