be8dd07523d6f7805df03ff72330356499bf8c90
Randh1r Gulraj <randhir.gulraj@the-future-group.com>
2025-10-09 12:08:57 +0200

    fix: fix sonarqube security issue (#1090)
    * fix: fix sonarqube security issue
    
    * chore: update docs/src/markdown/remark/links.js
    
    Co-authored-by: Rieven <rsnmartis@gmail.com>
    
    * fix: simplify regex patterns in iframe and links remark plugins
    
    * chore: update dependencies in `pnpm-lock.yaml`
    
    ---------
    
    Co-authored-by: Rieven <rsnmartis@gmail.com>

--- docs/src/markdown/remark/links.js
+++ docs/src/markdown/remark/links.js
@@ -1,24 +1,17 @@
 import { visit } from "unist-util-visit";
 
-const linkTagPattern = /<\s*?a\s+([^>]*?)>/g;
-const hrefPattern = /href=["']([^"']*?)["']/;
+const linkTagPattern = /<\/?a(?:\s[^>]*)?\s*>/g;
 
 /**
- * Remark the plugin to rewrite <a> to <Components.a> (for mdsvex).
+ * Remark plugin to rewrite <a> to <Components.a> (for mdsvex).
+ * @returns {(ast: Root) => undefined}
  */
 export function remarkLinks() {
   return function (ast) {
     visit(ast, "html", (node) => {
-      node.value = node.value.replaceAll(linkTagPattern, (match, attributes) => {
-        // Only replace, if href attribute exists
-        if (hrefPattern.test(attributes)) {
-          return match.replace("<a", "<Components.a");
-        }
-        return match;
-      });
-
-      // Also replace closing tags
-      node.value = node.value.replaceAll(/<\/\s*?a\s*?>/g, "</Components.a>");
+      node.value = node.value.replaceAll(linkTagPattern, (match) =>
+        match.replace(/a(?=\s|>)/, "Components.a"),
+      );
     });
   };
 }
