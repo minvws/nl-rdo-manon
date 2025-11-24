b1c57e0d63fabe2a8fd79512bd927f80e732bb30
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-11-24 08:03:34 +0100

    Add resizing based on conten to iframe component

--- docs/src/markdown/components/Iframe.svelte
+++ docs/src/markdown/components/Iframe.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import { onMount } from "svelte";
   import type { HTMLIframeAttributes } from "svelte/elements";
 
   let { src, children, ...rest }: HTMLIframeAttributes = $props();
   let rebasedSrc = $derived(src && src.startsWith("/") ? `${base}${src}` : src);
+
+  let iframeElement: HTMLIFrameElement;
+
+  function resizeIframe() {
+    if (iframeElement && iframeElement.contentWindow) {
+      iframeElement.style.height = `${iframeElement.contentWindow.document.body.scrollHeight}px`;
+    }
+  }
+
+  onMount(() => {
+    if (iframeElement) {
+      iframeElement.addEventListener("load", resizeIframe);
+      if (iframeElement.contentWindow) {
+        const resizeObserver = new ResizeObserver(resizeIframe);
+        resizeObserver.observe(iframeElement.contentWindow.document.body);
+      }
+    }
+  });
 </script>
 
-<iframe src={rebasedSrc} {...rest}>
+<iframe src={rebasedSrc} {...rest} bind:this={iframeElement}>
   {@render children?.()}
 </iframe>
