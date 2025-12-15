e9f84fc55ccb32c5a13058b1e061b06fd1e2de1e
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-12-15 12:57:36 +0100

    perf(visreg): Increase Playwright workers in CI

--- visreg/playwright/playwright.config.ts
+++ visreg/playwright/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   reporter: [["github"], ["html", { open: "never", outputFolder: "report" }]],
   use: {
     baseURL: BASE_URL,
