78b1ff919be032ae7699ac6dfd1deec4864da284
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-12-15 06:01:47 +0100

    Restructure

--- visreg/playwright/playwright.config.ts
+++ visreg/playwright/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['html', { open: 'never', outputFolder: './snapshots' }]],
+  reporter: [['html', { open: 'never', outputFolder: 'report' }]],
   use: {
     baseURL: BASE_URL,
     trace: 'on-first-retry',
