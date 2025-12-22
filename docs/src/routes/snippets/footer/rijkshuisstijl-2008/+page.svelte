e353d0e6975a5de299b4a63a5a3bd89b4acad5e9
Heleen <magned@gmail.com>
2025-12-22 15:06:25 +0100

    Add theme styling

---
+++ docs/src/routes/snippets/footer/rijkshuisstijl-2008/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { base } from "$app/paths";
+  const baseUrl = `${base}/examples/footer`;
+</script>
+
+<footer>
+  <span class="slogan">De Rijksoverheid. Voor Nederland</span>
+  <nav aria-labelledby="footer-nav-1-heading">
+    <h3 id="footer-nav-1-heading">Lorem ipsum</h3>
+    <ul>
+      <li><a href="#" aria-current="page">Dolor</a></li>
+      <li><a href="#">Sit</a></li>
+      <li><a href="#">Amet</a></li>
+    </ul>
+  </nav>
+</footer>
---
+++ docs/src/routes/snippets/skip-to-content/rijkshuisstijl-2008/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { page } from "$app/state";
+</script>
+
+<header>
+  <a href="#main-content" class="button focus-only skip-to-content">Ga direct naar inhoud</a>
+  <a
+    href={page.url.pathname}
+    class="logo"
+    aria-label="Logo bedrijfs- of product-naam, ga naar de homepage van bedrijfs- of product-naam"
+  >
+    <img src="$img/ro-logo.svg" alt="Logo Rijksoverheid" />
+    Rijksoverheid
+  </a>
+
+  <div class="content-wrapper">
+    <nav
+      data-open-label="Menu"
+      data-close-label="Sluit menu"
+      data-media="(min-width: 30rem)"
+      aria-label="Hoofdnavigatie"
+      class="collapsible"
+    >
+      <div class="collapsing-element">
+        <ul>
+          <li><a href={page.url.pathname}>Voorbeeld-link 1</a></li>
+          <li><a href={page.url.pathname}>Voorbeeld-link 2</a></li>
+          <li><a href={page.url.pathname}>Voorbeeld-link 3</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</header>
+
+<main id="main-content" tabindex="-1">
+  <!-- page content -->
+</main>
