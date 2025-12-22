292f2254f9c799fc1a0e108e4868b63a43d9cde4
JP Bruins Slot <jpbruinsslot@gmail.com>
2025-12-22 14:51:14 +0100

    chore: remove commented out playwright browser configurations

--- visreg/playwright/playwright.config.ts
+++ visreg/playwright/playwright.config.ts
@@ -18,14 +18,6 @@ export default defineConfig({
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
-    },
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    }
   ],
 });
