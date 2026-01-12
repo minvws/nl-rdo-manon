291a7bb5f616021b90178b3704c3025021067fb6
HeleenSG <magned@gmail.com>
2026-01-12 11:50:44 +0100

    Adds header icon styling (#1312)

--- docs/src/lib/SiteNavLinks.svelte
+++ docs/src/lib/SiteNavLinks.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <ul {...props}>
-  <li><NavLink href="{base}/">Home</NavLink></li>
+  <li><NavLink href="{base}/"><span class="icon icon-home">Hoofdpagina</span> Home</NavLink></li>
   <li><NavLink href="{base}/getting-started">Aan de slag</NavLink></li>
   <li><NavLink href="{base}/library">Bibliotheek</NavLink></li>
   <li><NavLink href="{base}/utility">Utility Classes</NavLink></li>
